9f9b4786977d85cd488ae9457340cb3a27204742
jobpassion <jobpassion@gmail.com>
2026-09-17 01:02:53 +0800

    Wire iOS-native new project and export handlers

--- webpack.ios.js
+++ webpack.ios.js
@@ -72,6 +72,14 @@ module.exports = makeConfig(
                 path.resolve(__dirname, 'src/renderer/ios/ScratchMenu.jsx')
             ),
             new webpack.NormalModuleReplacementPlugin(
+                /containers[\\/]menu-bar-hoc\.jsx$/,
+                path.resolve(__dirname, 'src/renderer/ios/IOSMenuBarHOC.jsx')
+            ),
+            new webpack.NormalModuleReplacementPlugin(
+                /lib[\\/]download-blob$/,
+                path.resolve(__dirname, 'src/renderer/ios/download-blob.js')
+            ),
+            new webpack.NormalModuleReplacementPlugin(
                 /scratch-logo(-android)?\.svg$/,
                 resource => {
                     resource.request = path.resolve(__dirname, 'src/renderer/assets/yiyi-menu-logo.svg');
